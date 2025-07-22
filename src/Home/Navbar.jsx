2cb0ad83d892532967362701ac84c86114d87798
Shahryar70 <mshahryar408@gmail.com>
2025-07-22 15:32:40 +0500

    Update Navbar.jsx

--- src/Home/Navbar.jsx
+++ src/Home/Navbar.jsx
@@ -106,8 +106,8 @@ export const Navbar = ({ withTop = false }) => {
             {/* Drawer Content */}
             <div className="flex-1 overflow-y-auto p-4 space-y-4">
               <a href="/" className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">Home</a>
-              <a href="/patients" className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">For Patients</a>
-              <a href="/donors" className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">For Donors</a>
+              <a className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">For Patients</a>
+              <a href="/donors" className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">Donation</a>
               <a href="/get-involved" className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">Get Involved</a>
               <a href="/about" className="block py-3 text-lg font-medium hover:text-red-600 border-b border-gray-100">About Us</a>
               
